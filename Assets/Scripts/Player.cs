73cff9e64100c18ca539bb6ab0e5dfbdac354f17
blank247d <blank247d@outlook.com>
2023-09-18 22:40:53 +0800

    physic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Windows;
-using Input = UnityEngine.Input;
 
 public class Player : MonoBehaviour {
 
@@ -16,7 +15,21 @@ public class Player : MonoBehaviour {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDirection = new(inputVector.x, 0, inputVector.y);
-        transform.position += moveSpeed * Time.deltaTime * moveDirection;
+
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = .7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirection, moveDistance);
+
+        if (!canMove) {
+            Vector3 castStart = transform.position + Vector3.up * playerHeight;
+            Vector3 castEnd = castStart + moveDirection * moveDistance;
+            Debug.DrawLine(castStart, castEnd, Color.red);
+        }
+
+        if (canMove) {
+            transform.position += moveDistance * moveDirection;
+        }
 
         float rotateSpeed = 20f;
         transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
